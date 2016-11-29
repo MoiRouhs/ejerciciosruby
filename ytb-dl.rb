119277909a3f7f4fe5e5a90c165e49a7783e5036
Moi Rouhs <carsrouhs@gmail.com>
2016-11-29 00:03:37 +0000

    creando la funcion de validacion de entrada

--- ytb-dl.rb
+++ ytb-dl.rb
@@ -1,4 +1,28 @@
 # En construccion
+formato = 0
+arreglo = Array.new
+
+#-------------Funcion para saber si es un enlace valido o no --------------------------#
+def validarenlace(entrada)
+    if (entrada.include? 'https://www.youtube.com/watch?') == true 
+        return true
+    else
+        return false
+    end
+end
+
+#-------------Funcion desanbiguacion --------------------------#
+def analisis (entrada)
+    case entrada
+    when true
+    
+    when false
+    
+    else
+    
+    end 
+end
+
 
 def formato (link)
    tres = system('youtube-dl -F ' + link)
@@ -8,6 +32,7 @@ end
 def descargar (numero,link)
     system('youtube-dl --format '+numero+' '+link)
 end
+#*********** UI ****************
 print 'Script para descarga de Youtube-dl no olvides debes tenerlo instalado para usarlo'
 print "Introdusca URL ===> "
 enlace = gets.chomp
