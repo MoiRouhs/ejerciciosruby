95ebf7eac9bfdedb20d80119627f89f229249247
Carlos Rocha <MoiRouhs@users.noreply.github.com>
2017-06-01 12:29:27 -0500

    Update ejemplo1.rb

--- jruby_art/ejemplo1.rb
+++ jruby_art/ejemplo1.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: false
 
 class Ejemplo1 < Processing::App
-  def settings # Método settings.
-    size 500, 500 # tamaño de la ventana.
+  def settings 
+    size 500, 500
   end
 
-  def setup # Método setup
-    sketch_title 'Ejemplo1' # Nombre del sketch. 
-    frame_rate 76 # Número de frames por segundo. 
+  def setup 
+    sketch_title 'Ejemplo1'  
+    frame_rate 76  
   end
 
-  def draw # Método draw 
-		background 0 # una de las caracteristicas de la ventana, en este caso el color de fondo de pantalla.
+  def draw  
+	background 0
   end
 end
