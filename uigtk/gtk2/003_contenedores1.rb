32ac7a3f99ca8a5ed55d94f7cf3176dd1699f7bb
Moi Rouhs <carsrouhs@gmail.com>
2018-06-30 01:06:35 +0000

    Primer ejemplo de contenedores correcciones

--- uigtk/gtk2/003_contenedores1.rb
+++ uigtk/gtk2/003_contenedores1.rb
@@ -17,20 +17,20 @@ boton1.signal_connect('clicked') do
 end
 
 boton2 = Gtk::Button.new("Botón 2")
-boton2.border_width = 5 # Borde alrededor del botón.
 boton2.signal_connect('clicked') do
 	puts "Botón 2 en caja"
 end
 
 ## Contenedor horizontal "HBox" y vertical "VBox"
 
-caja1 = Gtk::HBox.new(false,0) # Creación del Contenedor Hbox para horizontal y VBox para vertical.
+# Creación del Contenedor Hbox para horizontal y VBox para vertical, El 20 hace referencia al espacio entre elementos.
+caja1 = Gtk::HBox.new(true, 20) 
 
 ventana.add caja1 # Añadiendo el Contenedor a la ventana
 
-caja1.pack_start(boton1, false, false, 10) # Añadiendo el botón a el Contenedor.
+caja1.pack_start(boton1, true, true, 5) # Añadiendo el botón a el Contenedor.
 # pack_start(objeto_contenido, centra_objeto, objeto_como_relleno, padding)
-caja1.pack_start(boton2, true, true, 0)
+caja1.pack_start(boton2, true, true, 5)
 
 ventana.show_all
 Gtk.main
